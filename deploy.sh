13ab8d6028f5a424468ba5dfcd31413b2f63911c
Smalin0020 <dinglin0020@gmail.com>
2019-01-15 22:00:04 +0800

    modif deploy config

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:Smalin0020/year-end.git master:gh-pages
-git push -f origin gh-pages
+git push -f git@github.com:Smalin0020/year-end.git master:gh-pages
 
 cd -
